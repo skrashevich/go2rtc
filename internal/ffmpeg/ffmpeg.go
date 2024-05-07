a1327083081e471e705cc74349e1ac085a480152
Sergey Krashevich <svk@svk.su>
2024-05-07 08:17:14 +0300

    feat(ffmpeg): allow manual FFmpeg version specification in config
    This commit introduces the ability to manually specify the FFmpeg version within the application's configuration, rather than solely relying on automatic detection. This enhancement facilitates greater control over the application's behavior when working with specific FFmpeg versions, especially in environments where automatic version detection might be unreliable or undesirable. Additionally, a default version ("6.0") is now set in the configuration defaults, ensuring consistent behavior across different setups.

--- internal/ffmpeg/ffmpeg.go
+++ internal/ffmpeg/ffmpeg.go
@@ -34,7 +34,11 @@ func Init() {
 		return "exec:" + args.String(), nil
 	})
 
-	app.FFmpegVersion, _ = parseArgs("").GetFFmpegVersion()
+	if cfg.Mod["version"] == "6.0-default" {
+		app.FFmpegVersion, _ = parseArgs("").GetFFmpegVersion()
+	} else {
+		app.FFmpegVersion = cfg.Mod["version"]
+	}
 	log.Info().Str("version", app.FFmpegVersion).Msg("[ffmpeg] found")
 
 	if core.CompareVersions(app.FFmpegVersion, "7.0") >= 0 {
@@ -54,8 +58,9 @@ func Init() {
 }
 
 var defaults = map[string]string{
-	"bin":    "ffmpeg",
-	"global": "-hide_banner",
+	"bin":     "ffmpeg",
+	"version": "6.0-default",
+	"global":  "-hide_banner",
 
 	// inputs
 	"file": "-re -i {input}",
