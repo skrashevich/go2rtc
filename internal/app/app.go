15d2585507ae23675bf22c613a92d9a86d8963bd
Sergey Krashevich <svk@svk.su>
2024-05-09 06:12:57 +0300

    chore(version): bump app version to 1.9.1.1

--- internal/app/app.go
+++ internal/app/app.go
@@ -15,7 +15,7 @@ import (
 	"github.com/rs/zerolog/log"
 )
 
-var Version = "1.9.1"
+var Version = "1.9.1.1"
 var UserAgent = "go2rtc/" + Version
 var FFmpegVersion = ""
 var DefaultConfigFileName = "go2rtc.yaml"
